e236a02d724f5378af4ab76dabf5f42b44924b37
Sullivan <sxiong5307@gmail.com>
2019-04-03 18:40:57 -0700

    These are new changes

--- javascript/nav-bar.js
+++ javascript/nav-bar.js
@@ -5,12 +5,12 @@ class NavBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav class="nav-bar">
-                <a class="left" href="/aboutMe"><div class="nav-bar li" id="aboutMe">ABOUT ME</div></a>
-                <a class="left" href="/personalProjects"><div class="nav-bar li" id="personalProjects">PERSONAL PROJECTS</div></a>
-                <a class="left" href="/resume"><div class="nav-bar li" id="resume">RESUME</div></a>
+                <a class="leftNav" href="/aboutMe"><div class="nav-bar li" id="aboutMe">ABOUT ME</div></a>
+                <a class="leftNav" href="/personalProjects"><div class="nav-bar li" id="personalProjects">PERSONAL PROJECTS</div></a>
+                <a class="leftNav" href="/resume"><div class="nav-bar li" id="resume">RESUME</div></a>
                 <a href="/"><div class="nav-bar li" style="line-height: 0;"><img class="nav-bar home-img" src="/assets/homeImage.png" alt="homeImage"></div></a>
-                <a class="right" href="/coms101E-Portfolio"><div class="nav-bar li" id="coms101E-Portfolio">COMS101 PORTFOLIO</div></a>
-                <a class="right" href="/eng134E-Portfolio"><div class="nav-bar li" id="eng134E-Portfolio">ENG134 PORTFOLIO</div></a>
+                <a class="rightNav" href="/coms101E-Portfolio"><div class="nav-bar li" id="coms101E-Portfolio">COMS101 PORTFOLIO</div></a>
+                <a class="rightNav" href="/eng134E-Portfolio"><div class="nav-bar li" id="eng134E-Portfolio">ENG134 PORTFOLIO</div></a>
                 <a><div class="nav-bar li">WIP</div></a>
             </nav>
         `;
