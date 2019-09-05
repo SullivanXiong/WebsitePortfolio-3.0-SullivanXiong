f16d60818cd65ff275a8ced3c236b15e67cb5666
Sullivan <sxiong5307@gmail.com>
2019-09-05 16:50:21 -0700

    Angular node_modules

---
+++ python/maplestory_prices.py
@@ -0,0 +1,11 @@
+# chasiubaoo gave me 10mil when i started.
+
+input("What's the item you're looking for?: ")
+
+class item:
+    def __init__(self, category, name):
+        self.category = category
+        self.name = name
+
+zombie_tooth = item("etc", "Zombie's Lost Tooth")
+
